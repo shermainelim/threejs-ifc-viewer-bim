41611ed62d39505677d600fab4b507f8b0c92d67
shermainelim <shermaine.lim.si.hui@gmail.com>
2023-03-15 14:41:03 +0800

    popup box

--- examples/web-ifc-viewer/picking/bundle.js
+++ examples/web-ifc-viewer/picking/bundle.js
@@ -113718,7 +113718,7 @@ async function loadIfc(url) {
     const model = await viewer.IFC.loadIfcUrl(url);
     viewer.shadowDropper.renderShadow(model.modelID);
 }
-loadIfc('../../../IFC/01.ifc');
+loadIfc('../../../IFC/02.ifc');
 
 window.onmousemove = () => viewer.IFC.selector.prePickIfcItem(true);
 
@@ -113737,12 +113737,67 @@ window.onkeydown = (event) => {
     }
 };
 
-document.getElementById('express_22492')
+document.getElementById('express_803')
 .addEventListener('click', () => {
-    viewer.IFC.selector.pickIfcItemsByID(0, [22492], true);
+    viewer.IFC.selector.pickIfcItemsByID(0, [803], true);
 });
 
-document.getElementById('express_6518')
+document.getElementById('express_350')
 .addEventListener('click', () => {
-    viewer.IFC.selector.pickIfcItemsByID(0, [6518], true);
+    viewer.IFC.selector.pickIfcItemsByID(0, [350], true);
 });
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("express_803");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+
+// new modal 
+
+// Get the modal
+var modal = document.getElementById("myModal2");
+
+// Get the button that opens the modal
+var btn = document.getElementById("express_350");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close2")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
